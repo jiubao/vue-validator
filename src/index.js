b401782f8d874b41df4b83f96fd31939bd473716
xiadong8 <xiadong8@wanda.cn>
2018-02-21 10:48:39 +0800

    comments

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ export default { install }
 // 1. rm install
 // 2. cannot trigger input if not on input/textarea
 // 3. cannot trigger input if changed by code
+// 4. readme
+// 5. employee$name
+// 6. directive update
+// 7. destroy validators when component unmounted
+// 8. *.es *.umd *.cjs
+// 9. jest
+// 10. 
